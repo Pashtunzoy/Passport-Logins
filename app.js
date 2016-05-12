c985c1286554f4d12f28647837d0ff59b23dd448
Pashtunzoy <asifkhannoorzai@gmail.com>
2016-05-12 20:00:30 +1000

    Add the neccary requirements to get the Facebook Auth working.

--- app.js
+++ app.js
@@ -4,22 +4,27 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var passport = require('passport');
+var session = require('express-session');
+var expressLayouts=require("express-ejs-layouts");
+// Our Routes to Index, User, Auth
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var auth = require('./routes/auth');
 
+// App Intialisation
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressLayouts);
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
@@ -27,9 +32,20 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+// app.disable('etag');
+// The secret is just needed by the express session and is not going to be used anywhere.
+app.use(session({secret: 'your secret'}));
 
+// Connecting Passport and passing the app instance
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport.js')(app);
+
+
+// Making use of our routes
 app.use('/', routes);
 app.use('/users', users);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
