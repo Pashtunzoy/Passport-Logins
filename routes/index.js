313c130aa948db29a14f2cd2fac5b8f56107c8c3
Pashtunzoy <asifkhannoorzai@gmail.com>
2016-05-12 20:01:22 +1000

    Added the users route

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 
-router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Express'});
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Facebook Login' });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+
+// router.use('/', function(req, res, next){
+//   if(!req.user){
+//       res.redirect('/');
+//   }
+//   next();
+// });
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+  console.log(req.user);
+  res.render('users', {
+  user:  req.user
+  });
 });
+
 
 module.exports = router;
