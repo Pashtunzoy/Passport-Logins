58543a33e71b6933a2f27aec3cbcc14831035126
Pashtunzoy <asifkhannoorzai@gmail.com>
2016-05-13 20:45:01 +1000

    Changed the ID's path so they can be obtained from process.env

--- config/strategies/facebook.strategy.js
+++ config/strategies/facebook.strategy.js
@@ -3,9 +3,9 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 
 module.exports = function() {
   passport.use(new FacebookStrategy({
-    clientID: '269058713433032',
-    clientSecret: '5ee0b37d29a8731620b8829e1e6f70a4',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    clientID: process.env.FACEBOOK_CLIENT,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK_URL,
     passReqToCallback: true,
     profileFields: ['emails', 'name']
   },
