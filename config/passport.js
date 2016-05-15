7c8bebb62a32c9f68da0d38aa300301739741745
Pashtunzoy <asifkhannoorzai@gmail.com>
2016-05-15 20:52:13 +1000

    Added local login strategy require call

--- config/passport.js
+++ config/passport.js
@@ -1,17 +1,21 @@
 var passport = require('passport');
+var User = require('../models/userModel.js');
 
 module.exports = function(app) {
 
   passport.serializeUser(function(user, done) {
-    done(null, user);
+    done(null, user.id);
   });
 
-  passport.deserializeUser(function(user, done) {
-    done(null, user);
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
   });
   require('./strategies/facebook.strategy.js')();
   require('./strategies/google.strategy.js')();
   require('./strategies/twitter.strategy.js')();
   require('./strategies/github.strategy.js')();
   require('./strategies/local.strategy.js')();
+  require('./strategies/local-signin.strategy.js')();
 };
