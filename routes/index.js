d9c8295404ddadb93cca35ea6ee98e1ee3ccfcc8
Pashtunzoy <asifkhannoorzai@gmail.com>
2016-05-15 21:03:25 +1000

    Added some new routes for handling login and also added connect-flash messages on flasherror

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,45 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
-/* GET home page. */
+var User = require('../models/userModel.js');
+
+router.use(function(req,res,next){
+  res.locals.login = req.isAuthenticated();
+  res.locals.user = req.user;
+  next();
+});
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Facebook Login' });
+    res.render('index');
 });
 
 router.get('/login', function(req, res) {
-  res.render('signin');
+  if(req.user) {
+    return res.redirect('/users');
+  }
+  res.render('signin', { message: req.flash('loginMessage') });
 });
 
 router.get('/signup', function(req, res) {
-  res.render('signup');
+  res.render('signup', { message: req.flash('signupMessage') });
 });
+
+router.post('/login', passport.authenticate('local-signin', {
+  successRedirect : '/users',
+  failureRedirect : '/login',
+  failureFlash : true
+}));
 
 router.post('/signup',
   passport.authenticate('local', {
     successRedirect: '/users',
-    failureRedirect: '/signup'
+    failureRedirect: '/signup',
+    failureFlash: true
 }));
+
+router.get('/signout', function(req, res){
+    req.logout();
+    res.redirect('/login');
+});
 
 module.exports = router;
