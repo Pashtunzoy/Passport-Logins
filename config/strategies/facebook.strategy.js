7780d29dd7549c2162095223c26193e5d2c3d88d
Pashtunzoy <asifkhannoorzai@gmail.com>
2016-05-15 20:53:20 +1000

    Connected Facebook provider to any other Auth provider while logged in.

--- config/strategies/facebook.strategy.js
+++ config/strategies/facebook.strategy.js
@@ -1,5 +1,6 @@
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
+var User = require('../../models/userModel.js');
 
 module.exports = function() {
   passport.use(new FacebookStrategy({
@@ -11,14 +12,56 @@ module.exports = function() {
   },
   function(req, accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
-      var user = {};
-        user.email = profile.emails[0].value;
-        //user.image = profile._json.image.url;
-        user.displayName = profile.name.givenName+ ' ' + profile.name.familyName;
-        user.facebook = {};
-        user.facebook.id = profile.id;
-        user.facebook.token = accessToken;
-    done(null, user);
+      if (req.user) {
+          var query = {};
+          if (req.user.google) {
+              var query = {
+                  'google.id': req.user.google.id
+              };
+          } else if (req.user.twitter) {
+              var query = {
+                  'twitter.id': req.user.twitter.id
+              };
+          } else if (req.user.github) {
+              var query = {
+                  'github.id': req.user.github.id
+              };
+          } else if (req.user.local) {
+              var query = {
+                  'local.email': req.user.local.email
+              };
+          }
+          User.findOne(query, function (error, user) {
+              console.log(error);
+              if (user) {
+                user.facebook = {};
+                user.facebook.name = profile.name.givenName+ ' ' + profile.name.familyName;
+                user.facebook.email = profile.emails[0].value;
+                user.facebook.id = profile.id;
+                user.facebook.token = accessToken;
+                user.save();
+                done(null, user);
+              }
+          });
+      } else {
+        var query = {
+          'facebook.id': profile.id
+        };
+        User.findOne(query, function(err, user) {
+          if (user) {
+            done(null, user);
+          } else {
+            var user = new User();
+            user.facebook = {};
+            user.facebook.name = profile.name.givenName+ ' ' + profile.name.familyName;
+            user.facebook.email = profile.emails[0].value;
+            user.facebook.id = profile.id;
+            user.facebook.token = accessToken;
+            user.save();
+            done(null, user);
+          }
+        });
+      }
     });
   }));
 };
