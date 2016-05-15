a79ef01992f73718b57b6cf94d6de1df67a31c97
Pashtunzoy <asifkhannoorzai@gmail.com>
2016-05-15 20:54:42 +1000

    Connected Github  provider to any other Auth provider while logged in.

--- config/strategies/github.strategy.js
+++ config/strategies/github.strategy.js
@@ -1,5 +1,6 @@
 var passport = require('passport');
 var GitHubStrategy = require('passport-github2').Strategy;
+var User = require('../../models/userModel.js');
 
 module.exports = function() {
   passport.use(new GitHubStrategy({
@@ -10,15 +11,54 @@ module.exports = function() {
     },
     function(req, accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
-      var user = {};
-        // user.email = profile;
-        user.image = profile._json.avatar_url;
-        user.displayName = profile._json.name;
-        user.github = {};
-        user.github.id = profile.id;
-        user.github.token = accessToken;
-        // console.log(user);
-    done(null, user);
+      if (req.user) {
+        var query = {};
+        if(req.user.google) {
+            var query = {
+                'google.id': req.user.google.id
+            };
+        } else if(req.user.facebook) {
+            var query = {
+                'facebook.id': req.user.facebook.id
+            };
+        } else if (req.user.twitter) {
+            var query = {
+                'twitter.id': req.user.twitter.id
+            };
+        } else if (req.user.local) {
+            var query = {
+              'local.email': req.user.local.email
+            };
+        }
+        User.findOne(query, function (error, user) {
+            console.log(error);
+            if(user) {
+              user.github = {};
+              user.github.name = profile._json.name;
+              user.github.id = profile.id;
+              user.github.token = accessToken;
+              user.save();
+              done(null, user);
+            }
+        });
+      } else {
+        var query = {
+          'github.id': profile.id
+        };
+        User.findOne(query, function(err, user) {
+          if(user) {
+            done(null, user);
+          } else {
+            var user = new User();
+            user.github = {};
+            user.github.name = profile._json.name;
+            user.github.id = profile.id;
+            user.github.token = accessToken;
+            user.save();
+            done(null, user);
+          }
+        });
+      }
     });
   }));
 };
