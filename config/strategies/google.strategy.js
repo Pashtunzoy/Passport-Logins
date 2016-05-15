f41a3ec5c57295c3a4a69955aa3315f88e538b7e
Pashtunzoy <asifkhannoorzai@gmail.com>
2016-05-15 20:54:07 +1000

    Connected Google  provider to any other Auth provider while logged in.

--- config/strategies/google.strategy.js
+++ config/strategies/google.strategy.js
@@ -1,5 +1,6 @@
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var User = require('../../models/userModel.js');
 
 module.exports = function() {
   passport.use(new GoogleStrategy({
@@ -9,14 +10,57 @@ module.exports = function() {
   },
   function(req, accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
-      var user = {};
-        user.email = profile.emails[0].value;
-        user.image = profile._json.image.url;
-        user.displayName = profile.name.givenName+ ' ' + profile.name.familyName;
-        user.google = {};
-        user.google.id = profile.id;
-        user.google.token = accessToken;
-    done(null, user);
+      if (req.user) {
+          var query = {};
+          if (req.user.local) {
+              var query = {
+                'local.email': req.user.local.email
+              };
+          } else if(req.user.facebook) {
+              var query = {
+                'facebook.id': req.user.facebook.id
+              };
+          } else if (req.user.twitter) {
+                var query = {
+                  'twitter.id': req.user.twitter.id
+                };
+          } else if (req.user.github) {
+              var query = {
+                'github.id': req.user.github.id
+              };
+          }
+          User.findOne(query, function (error, user) {
+              console.log(error);
+              if (user) {
+                user.google = {};
+                user.google.id = profile.id;
+                user.google.email = profile.emails[0].value;
+                user.google.name = profile.name.givenName+ ' ' + profile.name.familyName;
+                user.google.token = accessToken;
+                user.save();
+                done(null, user);
+              }
+          });
+      } else {
+        var query = {
+          'google.id': profile.id
+        };
+
+        User.findOne(query, function(err, user) {
+          if (user) {
+            done(null, user);
+          } else {
+            var user = new User();
+            user.google = {};
+            user.google.id = profile.id;
+            user.google.email = profile.emails[0].value;
+            user.google.name = profile.name.givenName+ ' ' + profile.name.familyName;
+            user.google.token = accessToken;
+            user.save();
+            done(null, user);
+          }
+        });
+      }
     });
   }));
 };
