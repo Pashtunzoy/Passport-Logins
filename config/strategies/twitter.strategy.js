a0f8945edc4f167d5ad90ce6b0937c6599df8b49
Pashtunzoy <asifkhannoorzai@gmail.com>
2016-05-15 20:53:48 +1000

    Connected Twitter provider to any other Auth provider while logged in.

--- config/strategies/twitter.strategy.js
+++ config/strategies/twitter.strategy.js
@@ -1,5 +1,7 @@
 var passport = require('passport');
 var TwitterStrategy = require('passport-twitter').Strategy;
+var User = require('../../models/userModel.js');
+
 module.exports = function() {
 passport.use(new TwitterStrategy({
     consumerKey: process.env.TWITTER_CONSUMER_KEY,
@@ -9,14 +11,58 @@ passport.use(new TwitterStrategy({
   },
   function(req, token, tokenSecret, profile, done) {
     process.nextTick(function () {
-      var user = {};
-        // user.email = profile.emails[0].value;
-        user.image = profile.photos[0].value.replace('_normal', '');
-        user.displayName = profile.displayName;
-        user.twitter = {};
-        user.twitter.id = profile.id;
-        user.twitter.token = token;
-    done(null, user);
+      if (req.user) {
+        var query = {};
+        if(req.user.google) {
+            var query = {
+                'google.id': req.user.google.id
+            };
+        } else if(req.user.facebook) {
+            var query = {
+                'facebook.id': req.user.facebook.id
+            };
+        } else if (req.user.github) {
+            var query = {
+                'github.id': req.user.github.id
+            };
+        } else if (req.user.local) {
+            var query = {
+              'local.email': req.user.local.email
+            };
+        }
+        User.findOne(query, function (error, user) {
+            console.log(error);
+            if(user) {
+              user.twitter = {};
+              user.twitter.id = profile.id;
+              user.twitter.name = profile.displayName;
+              user.twitter.username = profile.username;
+              user.twitter.token = token;
+              user.twitter.tokenSecret = tokenSecret;
+              user.save();
+              done(null, user);
+            }
+        });
+    } else {
+      var query = {
+        'twitter.id': profile.id
+      };
+      User.findOne(query, function(err, user) {
+        if (user) {
+          done(null, user);
+        } else {
+          var user = new User();
+          user.twitter = {};
+          user.twitter.id = profile.id;
+          user.twitter.name = profile.displayName;
+          user.twitter.username = profile.username;
+          user.twitter.token = token;
+          user.twitter.tokenSecret = tokenSecret;
+          user.save();
+          done(null, user);
+        }
+      });
+    }
     });
   }
 ));
