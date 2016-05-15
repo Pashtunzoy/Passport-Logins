baadbec78699ef8d7639e56698075cfe73648154
Pashtunzoy <asifkhannoorzai@gmail.com>
2016-05-15 20:55:07 +1000

    Connected Local provider to any other Auth provider while logged in.

--- config/strategies/local.strategy.js
+++ config/strategies/local.strategy.js
@@ -1,29 +1,61 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var User = require('../../models/userModel.js');
 
 module.exports = function() {
-  var user = {
-    email: 'asep@gmail.com',
-    name: 'Asif',
-    password: 'Asif1994'
-  };
-  passport.use(new LocalStrategy({
+  passport.use('local', new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password',
     passReqToCallback: true
   },
   function(req, email, password, done) {
-    console.log('it worked');
     process.nextTick(function () {
-      if(email === user.email && password === user.password) {
-        // user.email = profile;
-        // user.image = profile._json.avatar_url;
-        user.displayName = user.name;
-        user.email = user.email;
-        console.log(user);
-        return done(null, user);
+      if (req.user) {
+        var query = {};
+      if(req.user.facebook) {
+          var query = {
+              'facebook.id': req.user.facebook.id
+          };
+      } else if(req.user.google) {
+            var query = {
+                'google.id': req.user.google.id
+            };
+        } else if (req.user.github) {
+            var query = {
+                'github.id': req.user.github.id
+            };
+        } else if (req.user.twitter) {
+            var query = {
+                'twitter.id': req.user.twitter.id
+            };
+        }
+        User.findOne(query, function (error, user) {
+            console.log(error);
+            if(user) {
+              user.local.name = req.param('name');
+              user.local.email = email;
+              user.local.password = user.generateHash(password);
+              user.save();
+              done(null, user);
+            }
+        });
+      } else {
+        var query = {
+          'local.email': email
+        };
+        User.findOne(query, function(err, user) {
+          if(user) {
+            return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+          } else {
+            var user = new User();
+            user.local.name = req.param('name');
+            user.local.email = email;
+            user.local.password = user.generateHash(password);
+            user.save();
+            done(null, user);
+          }
+        });
       }
-      done(null, null);
     });
   }));
 };
