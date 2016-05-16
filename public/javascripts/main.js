74828b09d02c6aa7b3690656dcba196b5d715ad5
Pashtunzoy <asifkhannoorzai@gmail.com>
2016-05-16 14:27:14 +1000

    Added Mob nav and it's working and fixed the regular nav a little bit

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,42 +1,6 @@
-// $( document ).ready(function() {
-//   // $(".dropdown-button").dropdown();
-//   $(".button-collapse").sideNav();
-// });
-
-// Initialize collapse button
-// Initialize collapsible (uncomment the line below if you use the dropdown variation)
-// $('.collapsible').collapsible();
-
-// $('.button-collapse').sideNav({
-//       menuWidth: 300, // Default is 240
-//       edge: 'right', // Choose the horizontal origin
-//       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-//     }
-//   );
-
-
-  // $('.button-collapse').sideNav({
-  //     menuWidth: 300, // Default is 240
-  //     edge: 'right', // Choose the horizontal origin
-  //     closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-  //   }
-  // );
-
-
-    // Initialize collapse button
-    // $(".button-collapse").sideNav();
-    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-    //$('.collapsible').collapsible();
-
-  $('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 240
-      edge: 'left', // Choose the horizontal origin
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
-
-
-// // Show sideNav
-// $('.button-collapse').sideNav('show');
-// // Hide sideNav
-// $('.button-collapse').sideNav('hide');
+$('.button-collapse').sideNav({
+    menuWidth: 300,
+    edge: 'left',
+    closeOnClick: true
+  }
+);
