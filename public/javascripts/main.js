f32dcc7e6ab58c483ee7fa02c30d7ae95f330851
Pashtunzoy <asifkhannoorzai@gmail.com>
2016-05-16 13:55:15 +1000

    Made the side nav work on phones but still(WIP)

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,11 @@
-$( document ).ready(function() {
-  // $(".dropdown-button").dropdown();
-  $(".button-collapse").sideNav();
-});
+// $( document ).ready(function() {
+//   // $(".dropdown-button").dropdown();
+//   $(".button-collapse").sideNav();
+// });
 
 // Initialize collapse button
 // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-$('.collapsible').collapsible();
+// $('.collapsible').collapsible();
 
 // $('.button-collapse').sideNav({
 //       menuWidth: 300, // Default is 240
@@ -15,7 +15,28 @@ $('.collapsible').collapsible();
 //   );
 
 
-// Show sideNav
-$('.button-collapse').sideNav('show');
-// Hide sideNav
-$('.button-collapse').sideNav('hide');
+  // $('.button-collapse').sideNav({
+  //     menuWidth: 300, // Default is 240
+  //     edge: 'right', // Choose the horizontal origin
+  //     closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  //   }
+  // );
+
+
+    // Initialize collapse button
+    // $(".button-collapse").sideNav();
+    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+    //$('.collapsible').collapsible();
+
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+
+
+// // Show sideNav
+// $('.button-collapse').sideNav('show');
+// // Hide sideNav
+// $('.button-collapse').sideNav('hide');
